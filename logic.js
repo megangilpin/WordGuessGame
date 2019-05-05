0b39edbe4aa145b3d17718416ca834a1f35cdb65
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-05-05 18:28:18 -0400

    all part 1 passes test

--- logic.js
+++ logic.js
@@ -1,6 +1,12 @@
 // word to guess for word game - theme = WORDS
 var gameWords = ["woord","parola", "ord", "slowo", "word", "mot", "gair"];
 
+// var myGame ={
+//     word:ansWord,
+//     guessesLeft:9,
+//     wrongGuesses: [],
+//     puzzleState: getBlanks(ansWord),
+// }
 
 // Returns random word from gameWords array
 var randomWord = function (gameWords) {
@@ -24,21 +30,21 @@ var isCorrectGuess = function(word, letter) {
 
 // Fills ansWordArr with correct number of blanks equal to the randomWord
 var getBlanks = function(word) {
-   var ansWordArr = []
+   var ansWord = []
     for (var i = 0; i < word.length; i++) {
-        ansWordArr[i] = "_";
+        ansWord[i] = "_";
     }
-    return ansWordArr;
+    return ansWord;
 }
 
 // fills a blanks array in the correct locations given a letter and the word that array was built from
-var fillBlanks = function(word, ansCurrent, letter) {
+var fillBlanks = function(word, ansWord, letter) {
   for (var i=0; i<=word.length; i++) {
-  if (word[i] === letter){
-   ansCurrent[i] = letter
+    if (word[i] === letter){
+      ansWord[i] = letter
+    }
   }
-  }
-  return ansCurrent;
+  return ansWord;
 }
 
 //Manages the state of the game - aka the score for the current round and the over all game score
@@ -53,6 +59,99 @@ var setupRound = function(ansWord){
    return roundScore;
   }
 
-  
 
+var updateRound = function(round, letter){
+// if isCorrectGuess is true update puzzleState on the round, 
+  if( isCorrectGuess(round.word, letter)){
+    round.puzzleState = fillBlanks(round.word, round.puzzleState, letter);
+  }
+// if isCorrectGuess is false update wrong guess and guess left
+  else {
+    round.guessesLeft = round.guessesLeft - 1;
+    round.wrongGuesses.push(letter);
+  }
+}
+
+//checks if the game has been won or not
+// var hasWon = function(round){
+ //first check if the round has been won
+  // if(round.puzzleState = getBlanks(round)){
+  //   return false
+  // }
+  //  else if (round.puzzleState === round.word){
+  //    return true
+  //  }
+  //   }
+  var hasWon = function(puzzleState){
+    for (var i = 0; i< puzzleState.length; i++){
+      if (puzzleState[i] === "_"){
+        return false;
+      }
+    }
+    return true;
+  }
+
+    // if(round.puzzleState === round.word){
+    //   return true;
+    // }
+    // return false;
+    // }
+
+
+//checks if the game has been won or not
+// var hasLost = function(round){
+//   if( round.guessesLeft === 0)
+//    return youLost;
+// }
+
+var hasLost = function(guessesLeft){
+  if(guessesLeft === 0){
+    return true;
+  }
+   return false;
+}
+
+var isEndOfRound = function(round){
+  if(round.guessesLeft === 0){
+    return true;
+  }
+  if (hasWon(round.puzzleState)){
+    return true;
+  }
+  return false;
+}
+
+var setupGame = function(gameWords, win, loses){
+  var newRound = {
+    words: gameWords,
+    wins: win,
+    losses: loses,
+    round: setupRound(randomWord(gameWords)),
+  }
+  return newRound;
+}
+
+var startNewRound = function(game){
+  var puzzleState = game.round.puzzleState;
+    if (hasWon(puzzleState) === true) {
+        ++game.wins;
+        alert("You did it! The word is " + game.round.word);
+    }
+    else {
+        ++game.losses;
+        alert("Sorry, he word was " + game.round.word);
+    }
+    return game;
+}
+
+
+// var myGame ={
+//     word:ansWord,
+//     guessesLeft:9,
+//     wrongGuesses: [],
+//     puzzleState: getBlanks(ansWord),
+// }
+
+
+var myGame =setupGame(gameWords, 0, 0)
 
